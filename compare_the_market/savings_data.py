30cecf914aa6795bba708adcaae26d3e2da01fb2
Philip Kalinda <philipkalinda@gmail.com>
2016-06-10 17:05:45 +0100

    Update savings_data.py

--- compare_the_market/savings_data.py
+++ compare_the_market/savings_data.py
@@ -5,14 +5,16 @@ from bs4 import BeautifulSoup
 import json
 import pandas as pd
 
+#Web Scraping and Parsing
 response = requests.get('https://money.comparethemarket.com/savings-accounts/?AFFCLIE=CM01')
 parser = BeautifulSoup(response.content, 'html.parser')
-
 savings_json_data = parser.find_all('div', id='savings-json')[0]
+
+#Python Object Conversion
 savings_py_obj = json.loads(savings_json_data.text)
 
+#Column Naming
 cols = []
-
 for bank in savings_py_obj:
     for key,value in bank.items():
         if key not in cols:
@@ -20,7 +22,7 @@ for bank in savings_py_obj:
         else:
             continue
 
-#DataFrame
+#DataFrame Creation
 raw_data = pd.DataFrame(columns=cols)
 for bank in savings_py_obj:
     raw_data = raw_data.append(bank, ignore_index=True)
