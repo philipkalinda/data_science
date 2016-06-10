d53c87fd31baa852102ca9de294ab59691664b8a
Philip Kalinda <philipkalinda@gmail.com>
2016-06-10 15:46:51 +0100

    Create savings_data.py
    Script to extract all the savings accounts data available in order to carry out analysis

---
+++ compare_the_market/savings_data.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import pandas as pd
+
+response = requests.get('https://money.comparethemarket.com/savings-accounts/?AFFCLIE=CM01')
+parser = BeautifulSoup(response.content, 'html.parser')
+
+savings_json_data = parser.find_all('div', id='savings-json')[0]
+savings_py_obj = json.loads(savings_json_data.text)
+
+cols = []
+for key,value in savings_py_obj[0].items():
+    if key not in cols:
+        cols.append(key)
+    else:
+        continue
+
+raw_data = pd.DataFrame()
+
+
+for bank in savings_py_obj:
+    for key,value in bank.items():
+        pass
