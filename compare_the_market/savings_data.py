d57dc1ac3b6ff7186086bf9fa50b59f1b89be224
Philip Kalinda <philipkalinda@gmail.com>
2016-08-11 09:51:09 +0100

    database management addition WIP
    database management addition WIP

--- compare_the_market/savings_data.py
+++ compare_the_market/savings_data.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import pandas as pd
+import sqlite3
 
 #Web Scraping and Parsing
 response = requests.get('https://money.comparethemarket.com/savings-accounts/?AFFCLIE=CM01')
@@ -28,3 +29,24 @@ for bank in savings_py_obj:
     raw_data = raw_data.append(bank, ignore_index=True)
 
 print(raw_data)
+
+#Database Management
+
+conn = sqlite3.connect('savings_db.db')
+c = conn.cursor()
+c.execute("""drop table if exists towns""")
+conn.commit()
+
+# Create table
+c.execute('''CREATE TABLE savings_table
+             (Name TEXT, Id INTEGER, ProviderBrandName TEXT, IsInstantAccess TEXT, IsCashIsa TEXT, IsFixedRateBond TEXT,
+             ProductImage TEXT, ApplyLink TEXT, GrossAer REAL, RateType TEXT, IntroBonusNote TEXT, MinimumInvestment INTEGER,
+             MaximumInvestment INTEGER, ProductApplicationUrl TEXT, IsCommercial TEXT, HasInternetAccess TEXT, HasBranchAccess TEXT,
+             HasTelephoneAccess TEXT, HasPostAccess TEXT, SavingsType TEXT, AdditionalInfo TEXT, MinimumAge INTEGER,
+             MaximumAge INTEGER, Pros TEXT, Cons TEXT, GrossAerString TEXT, MinimumInvestmentString TEXT, MaximumInvestmentString TEXT,
+             EligibilityAgeString TEXT, HasImage TEXT)''')
+
+c.execute("INSERT INTO savings_table VALUES (#values#)", raw_data) # <- Enter your values here
+
+conn.commit()
+conn.close()
